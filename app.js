e653f83f1ab9f1d53242b15f27c57f83146f77ab
tomiwadotJS <victorgbayedev@gmail.com>
2023-10-19 11:21:15 +0100

    user schema and model

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 require('dotenv').config()
+require('express-async-errors')
 
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+
 //CONNECTDB
 const connectDB = require('./db/connect')
+
+//Middleware
+const notFoundMiddleWare = require('./middleware/not-found')
+const errorHandlerMiddleWare = require('./middleware/error-handler')
+
+app.use(morgan('tiny'))
+app.use(express.json())
 
 
 app.get('/', (req, res)=>{
     res.send('proform-api')
 })
+
+app.use(notFoundMiddleWare)
+app.use(errorHandlerMiddleWare)
 
 const port = process.env.PORT || 5000
 
