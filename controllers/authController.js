39fc763c4347a3752d8c8f1d31e0aa016ef7b6b5
tomiwadotJS <victorgbayedev@gmail.com>
2023-10-22 23:03:21 +0100

    auth route and controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,18 @@
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const register = async(req,res) => {
-    res.send('register user')
+    const { email, name, password } = req.body;
+
+    const emailAlreadyExists = await User.findOne({ email });
+    if (emailAlreadyExists) {
+      throw new CustomError.BadRequestError('Email already exists');
+    }
+
+    const user = await User.create({ name, email, password, role });
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async(req,res) => {
