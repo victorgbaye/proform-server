cb6fecedfcacb837757aae32862b4b99093b186e
tomiwadotJS <victorgbayedev@gmail.com>
2024-02-28 23:41:56 +0000

    changes

--- models/Form.js
+++ models/Form.js
@@ -11,7 +11,7 @@ const questionSchema = new mongoose.Schema({
     // _id: mongoose.Schema.Types.ObjectId,
     questionText: {
       type: String,
-      // required: true,
+      required: true,
       // maxLength: 
     },
     questionType: {
