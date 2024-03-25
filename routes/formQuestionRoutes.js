4b53e08c0cbdddd3002a84fcab6c6c71e4f89ce2
tomiwadotJS <victorgbayedev@gmail.com>
2024-03-25 09:31:18 +0000

    changes

--- routes/formQuestionRoutes.js
+++ routes/formQuestionRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { getAllQuestions, getSingleQuestion, addQuestion, updateQuestion, deleteQuestion } = require('../controllers/FormQuestionController');
+const { getAllQuestions, getSingleQuestion, addQuestion, updateQuestion, deleteQuestion } = require('../controllers/formQuestionController');
 
 router.route('/').post(addQuestion).get(getAllQuestions);
 router.route('/:questionId')
